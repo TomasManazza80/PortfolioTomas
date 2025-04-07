ea90094c1f45438c78f81fffe4798cb15102ffde
TomasManazza80 <tomas.manazza19@gmail.com>
2025-04-07 18:26:26 -0300

    package added

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -3,6 +3,9 @@ import { motion } from "framer-motion";
 import Logo from "../components/svg/Logo";
 import HeroBackground from "../components/background/HeroBackground";
 import React from "react";
+import Link from "next/link";
+import {SiGithub} from "react-icons/si";
+
 import imagen from '../../public/backgorund/tomas.png';
 const Hero = () => {
     return (
@@ -37,6 +40,19 @@ const Hero = () => {
     >
       Full Stack Developer
     </h2>
+
+    <Link
+                                    href={"https://github.com/TomasManazza80"}
+                                    target="_blank"
+                                    aria-label="Open GitHub Repository"
+                                    className="rounded-full w-[20px] bg-white p-5 text-[20px] md:w-[25px] md:text-[24px] lg:w-[65px] lg:text-[28px]"
+                                    data-blobity
+                                    data-blobity-radius="35"
+                                    data-blobity-offset-x="4"
+                                    data-blobity-offset-y="4"
+                                    data-blobity-magnetic="false">
+                                    <SiGithub/>
+                                </Link>
   </div>
 </div>
         </motion.section>
