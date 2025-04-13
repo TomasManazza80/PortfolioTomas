f419290e2f5013f8f0d945323bc2cf6d68217a36
TomasManazza80 <tomas.manazza19@gmail.com>
2025-04-12 21:17:09 -0300

    stack added

--- app/sections/Tools.tsx
+++ app/sections/Tools.tsx
@@ -1,85 +1,76 @@
-import React from "react";
-import "../animations/animate.css";
-import AnimatedBody from "../animations/AnimatedBody";
-import AnimatedTitle from "../animations/AnimatedTitle";
-import {
-    SiAdobeaftereffects, SiAdobefonts,
-    SiAdobeillustrator,
-    SiAdobephotoshop, SiAstro, SiCinema4D, SiCplusplus, SiFigma, SiFramer, SiGit,
-    SiGithub,
-    SiGooglechrome, SiGooglefonts,
-    SiIntellijidea, SiJavascript, SiKotlin, SiLua,
-    SiNeovim, SiNextdotjs, SiReact, SiRust, SiTypescript, SiWebstorm, SiZig
-} from "react-icons/si";
-import AnimatedTools from "../animations/AnimatedTools.tsx";
+import React from 'react';
+import { FaNodeJs, FaReact, FaGithub } from 'react-icons/fa';
+import { SiTypescript, SiPostgresql, SiTailwindcss, SiHtml5, SiVite, SiDocker , SiJavascript, SiExpress, SiNestjs } from 'react-icons/si';
 
 const Tools = () => {
-    return (
-        <section
-            className="relative z-10 w-full items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pt-16 pb-36 md:pt-20 md:pb-44 lg:pt-20 lg:pb-56"
-            id="tools"
-        >
-            <div className="mx-auto flex w-[90%] flex-col items-center justify-center lg:max-w-[1212.8px]">
-                <AnimatedTitle
-                    text={"LENGUAJES."}
-                    className={
-                        "mb-10 text-left text-[40px] font-bold leading-[0.9em] tracking-tighter text-[#e4ded7] sm:text-[45px] md:mb-16 md:text-[60px] lg:text-[80px]"
-                    }
-                    wordSpace={"mr-[14px]"}
-                    charSpace={"mr-[0.001em]"}
-                />
+  return (
+    <section
+      className="relative z-10 w-full items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pt-16 pb-36"
+    >
+     <h1 className="text-center mb-[60px]">STACK DE TECNOLOGÍAS</h1>
+      <div className="flex flex-wrap justify-center">
+        <div className="flex flex-col items-center mx-4">
+          <FaNodeJs className="text-5xl text-white" />
+          <span className="text-lg text-white">Node.js</span>
+        </div>
+        <div className="flex flex-col items-center mx-4">
+          <FaReact className="text-5xl text-white" />
+          <span className="text-lg text-white">React</span>
+        </div>
+        <div className="flex flex-col items-center mx-4">
+          <SiNestjs className="text-5xl text-white" />
+          <span className="text-lg text-white">Nest.js</span>
+        </div>
+        <div className="flex flex-col items-center mx-4">
+          <SiTypescript className="text-5xl text-white" />
+          <span className="text-lg text-white">TypeScript</span>
+        </div>
+        <div className="flex flex-col items-center mx-4">
+          <SiJavascript className="text-5xl text-white" />
+          <span className="text-lg text-white">JavaScript</span>
+        </div>
+        <div className="flex flex-col items-center mx-4">
+          <SiExpress className="text-5xl text-white" />
+          <span className="text-lg text-white">Express</span>
+        </div>
 
-                <div className="mx-auto w-[100%] lg:max-w-[1200px] justify-center">
-                  
-                <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-bold leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[40px] md:leading-relaxed lg:mb-16 lg:w-[50%]">
-                        <AnimatedBody delay={0.3} text="Backend" />
-                        <div>
-                            <AnimatedTools className="grid grid-cols-5 gap-4" delay={0.3} stepSize={0.1} iconSize={50}>
-                                <SiZig size={50}/>
-                                <SiRust size={50}/>
-                                <SiCplusplus size={50}/>
-                                <SiLua size={50}/>
-                                <SiKotlin size={50}/>
-                            </AnimatedTools>
-                        </div>
-                    </div>
-                  
-                  
-                    
-                    <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-bold leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[40px] md:leading-relaxed lg:mb-16 lg:w-[50%]">
-                        <AnimatedBody delay={0.2} text="Frontend" />
-                        <div>
-                            <AnimatedTools className="grid grid-cols-5 gap-4" delay={0.2} stepSize={0.1} iconSize={50}>
-                                <SiTypescript size={50}/>
-                                <SiNextdotjs size={50}/>
-                                <SiJavascript size={50}/>
-                                <SiReact size={50}/>
-                                <SiAstro size={50}/>
-                                <SiNextdotjs size={50}/>
-                            </AnimatedTools>
-                        </div>
-                    </div>
-                 
-                    <div className="mb-10 flex w-[100%] flex-col gap-4 text-[18px] font-bold leading-relaxed tracking-wide text-[#e4ded7] md:mb-16 md:gap-6 md:text-[40px] md:leading-relaxed lg:mb-16 lg:w-[50%]">
-                        <AnimatedBody delay={0.4} text="Other" />
-                        <div>
-                            <AnimatedTools className="grid grid-cols-5 gap-4" delay={0.4} stepSize={0.1} iconSize={50}>
-                                <SiGithub size={50}/>
-                                <SiGit size={50}/>
-                                <SiNeovim size={50}/>
-                                <SiIntellijidea size={50}/>
-                                <SiWebstorm size={50}/>
-                            </AnimatedTools>
-                        </div>
-                    </div>
-                </div>
+        <div className="flex flex-col items-center mx-4">
+          <SiPostgresql className="text-5xl text-white" />
+          <span className="text-lg text-white">PostgreSQL</span>
+        </div>
+
+
+        <div className="flex flex-col items-center mx-4">
+          <SiVite className="text-5xl text-white" />
+          <span className="text-lg text-white">Vite</span>
+        </div>
+
+        <div className="flex flex-col items-center mx-4">
+          <SiDocker className="text-5xl text-white" />
+          <span className="text-lg text-white">Docker</span>
+        </div>
 
 
 
+        <div className="flex flex-col items-center mx-4">
+          <SiHtml5 className="text-5xl text-white" />
+          <span className="text-lg text-white">HTML</span>
+        </div>
 
-            </div>
-        </section>
-    );
+        <div className="flex flex-col items-center mx-4">
+          <FaGithub className="text-5xl text-white" />
+          <span className="text-lg text-white">GitHub</span>
+        </div>
+
+        <div className="flex flex-col items-center mx-4">
+          <SiTailwindcss className="text-5xl text-white" />
+          <span className="text-lg text-white">Tailwind</span>
+        </div>
+
+        {/* Agrega más herramientas aquí */}
+      </div>
+    </section>
+  );
 };
 
 export default Tools;
